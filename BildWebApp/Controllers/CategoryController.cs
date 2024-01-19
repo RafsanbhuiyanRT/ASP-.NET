1fb9e7cd2be67a352a3f8f2e51c7568824cee63f
RafsanBhuiyanRT <Rafsanbhuiyan01yt4@gmail.com>
2024-01-20 01:35:44 +0600

    connect sql server and write some frontend code

--- BildWebApp/Controllers/CategoryController.cs
+++ BildWebApp/Controllers/CategoryController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BildWebApp.Data;
+using BildWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BildWebApp.Controllers;
 public class CategoryController : Controller
 {
+    private readonly ApplicationDbContext _db;
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
     public IActionResult Index()
     {
-        return View();
+        List<Category> ObjCatagoryList = _db.Categories.ToList();
+        return View(ObjCatagoryList);
     }
 }
