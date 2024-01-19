84223392b1ea77f2560890be4fad9d5af5bdecf1
RafsanBhuiyanRT <Rafsanbhuiyan01yt4@gmail.com>
2024-01-19 21:28:26 +0600

    Added Category Controller

---
+++ BildWebApp/Controllers/CategoryController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BildWebApp.Controllers;
+public class CategoryController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
